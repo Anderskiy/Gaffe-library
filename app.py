720a203a16810c3f3f00347f762f5c9a6d9d3167
Anderskiy <ander.men70@gmail.com>
2024-09-23 21:58:53 +0300

    update index.html

--- app.py
+++ app.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/updates/')
+def updates():
+    return render_template('updates.html')
+
+@app.route('/resources/')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/help/')
+def help_page():
+    return render_template('help.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
